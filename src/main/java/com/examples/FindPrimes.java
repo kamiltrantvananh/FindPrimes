ddbf6b2a00970f666e1b4bfe32e03cfe3c85999f
Kamil Tran <kamil.tran@futurenet.com>
2020-09-21 15:57:37 +0200

    Initial commit

---
+++ src/main/java/com/examples/FindPrimes.java
@@ -0,0 +1,8 @@
+package com.examples;
+
+public class FindPrimes {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/com/examples/FindPrimesTest.java
@@ -0,0 +1,7 @@
+package com.examples;
+
+import org.junit.jupiter.api.Test;
+
+class FindPrimesTest {
+
+}
