6d04079a84fba60022bebc70ac2041c13aef5e89
Kamil Tran <kamil.tran@futurenet.com>
2020-09-30 10:47:08 +0200

    add test + fix

--- src/main/java/com/examples/FindPrimes.java
+++ src/main/java/com/examples/FindPrimes.java
@@ -12,21 +12,25 @@ import java.io.IOException;
 
 public class FindPrimes {
 
+    private static final int FIRST_SHEET = 0;
+    private static final int B_CELL = 1;
+    private static final int SECOND_ROW = 1;
+
     public static void main(String[] args) throws IOException {
         File file = getFile(args);
-        getNumbersFromFile(file);
+        getPrimesFromFile(file);
     }
 
-    private static void getNumbersFromFile(File file) throws IOException {
-        FileInputStream fis = new FileInputStream(file);
-        Workbook workbook = new XSSFWorkbook(fis);
-        try {
-            Sheet sheet = workbook.getSheetAt(0);
+    public static void getPrimesFromFile(File file) throws IOException {
+        try (FileInputStream fis = new FileInputStream(file);
+             Workbook workbook = new XSSFWorkbook(fis)) {
 
-            for (int rowIndex = 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
+            Sheet sheet = workbook.getSheetAt(FIRST_SHEET);
+
+            for (int rowIndex = SECOND_ROW; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
                 Row row = sheet.getRow(rowIndex);
                 if (row != null) {
-                    Cell cell = row.getCell(1);
+                    Cell cell = row.getCell(B_CELL);
                     if (cell != null) {
                         Long number = Long.valueOf(cell.getStringCellValue());
 
@@ -36,30 +40,22 @@ public class FindPrimes {
                     }
                 }
             }
-        } finally {
-            workbook.close();
         }
     }
 
-    private static File getFile(String[] args) {
-        if (args.length == 0) {
-            throw new IllegalArgumentException("Missing file path.");
-        } else if (args.length > 1) {
-            throw new IllegalArgumentException("Too many arguments.");
-        }
-
-        return new File(args[0]);
+    public static File getFile(String[] args) {
+        if (args.length == 0) throw new IllegalArgumentException("Missing file path.");
+        else if (args.length > 1) throw new IllegalArgumentException("Too many arguments.");
+        else return new File(args[0]);
     }
 
-    private static boolean isPrime(Long num) {
-        boolean flag = true;
+    public static boolean isPrime(Long num) {
+        if (num < 1) return false;
+
         for(int i = 2; i <= num/2; ++i) {
-            if(num % i == 0) {
-                flag = false;
-                break;
-            }
+            if(num % i == 0) return false;
         }
 
-        return flag;
+        return true;
     }
 }
--- src/test/java/com/examples/FindPrimesTest.java
+++ src/test/java/com/examples/FindPrimesTest.java
@@ -1,7 +1,82 @@
 package com.examples;
 
+import org.apache.poi.openxml4j.exceptions.NotOfficeXmlFileException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class FindPrimesTest {
 
+    private final PrintStream stdOut = System.out;
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(stdOut);
+    }
+
+    @Test
+    void testNoFile() {
+        Throwable ex = assertThrows(IllegalArgumentException.class,
+                () -> FindPrimes.getFile(new String[]{}));
+
+        assertEquals("Missing file path.", ex.getMessage());
+    }
+
+    @Test
+    void testTooManyArguments() {
+        Throwable ex = assertThrows(IllegalArgumentException.class,
+                () -> FindPrimes.getFile(new String[]{"one", "two"}));
+
+        assertEquals("Too many arguments.", ex.getMessage());
+    }
+
+    @Test
+    void testInvalidFile() {
+        File file = new File("src/test/resources/invalid.txt");
+
+        assertThrows(NotOfficeXmlFileException.class,
+                () -> FindPrimes.getPrimesFromFile(file));
+    }
+
+    @Test
+    void testNoNumbers() throws IOException {
+        File file = new File("src/test/resources/no_data.xlsx");
+
+        FindPrimes.getPrimesFromFile(file);
+
+        assertEquals("", outputStream.toString().trim());
+    }
+
+    @Test
+    void testNegativeNumbers() throws IOException {
+        File file = new File("src/test/resources/negative_numbers.xlsx");
+
+        FindPrimes.getPrimesFromFile(file);
+
+        assertEquals("1\n3", outputStream.toString().trim());
+    }
+
+    @Test
+    void testFindAllPrimes() throws IOException {
+        File file = new File("src/main/resources/vzorek_dat.xlsx");
+
+        FindPrimes.getPrimesFromFile(file);
+
+        String expected = "5645657\n15619\n1234187\n211\n7\n9788677\n23311\n54881";
+        assertEquals(expected, outputStream.toString().trim());
+    }
 }
